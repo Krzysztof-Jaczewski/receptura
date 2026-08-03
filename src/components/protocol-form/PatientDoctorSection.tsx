90b18ebcac5d4d7912115bfd37c5e8b04298b930
Krzysztof-Jaczewski <83280440+Krzysztof-Jaczewski@users.noreply.github.com>
2026-08-03 10:55:29 +0200

    Update PatientDoctorSection.tsx
    chcanged icon

--- src/components/protocol-form/PatientDoctorSection.tsx
+++ src/components/protocol-form/PatientDoctorSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { UseFormRegister } from 'react-hook-form';
-import { User, Stethoscope } from 'lucide-react';
+import { User, Stethoscope,Receipt-text } from 'lucide-react';
 
 import { ProtocolFormValues } from '@/lib/schemas/protocolSchema';
 
@@ -22,7 +22,7 @@ const PatientDoctorSection = ({ register }: Props) => {
         <div className='grid grid-cols-3 gap-4'>
  <div className={section}>
                 <div className='flex items-center gap-2 font-semibold'>
-                    <User className='text-green-600 w-5 h-5' />
+                    <Receipt-text className='text-red-600 w-5 h-5' />
                     Dane recepty
                 </div>
 
